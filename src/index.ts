0e00f49eca7c1e3268ac4c82e9de9795aa2ba476
Daniel Nigusse <nabute925@gmail.com>
2023-11-18 01:37:57 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+const showcase = document.getElementById('showcase') as HTMLIFrameElement;
+const key = '6k7wbe5kf66bmbqwqedwcxdkb';
+
+// declare this file is a module
+export {};
+
+// augment window with the MP_SDK property
+declare global {
+  interface Window {
+    MP_SDK: any;
+  }
+}
+
+showcase.addEventListener('load', async function() {
+  let sdk;
+  try {
+    sdk = await showcase.contentWindow.MP_SDK.connect(showcase, key, '3.6');
+  }
+  catch(e) {
+    console.error(e);
+    return;
+  }
+
+  console.log('%c  Hello Bundle SDK! ', 'background: #333333; color: #00dd00');
+  console.log(sdk);
+});
